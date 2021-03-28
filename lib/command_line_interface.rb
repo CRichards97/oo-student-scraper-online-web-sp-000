5eb1cdd2721c8300d2474ac6a39b59548a0f0a71
Clint Richards <richardsclint17@gmail.com>
2021-03-28 00:39:02 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -16,28 +16,28 @@ class CommandLineInterface
   require_relative "../lib/student.rb"
   require 'nokogiri'
   require 'colorize'
-  
+
   class CommandLineInterface
     BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
-  
+
     def run
       make_students
       add_attributes_to_students
       display_students
     end
-  
+
     def make_students
       students_array = Scraper.scrape_index_page(BASE_PATH + 'index.html')
       Student.create_from_collection(students_array)
     end
-  
+
     def add_attributes_to_students
       Student.all.each do |student|
         attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
         student.add_student_attributes(attributes)
       end
     end
-  
+
     def display_students
       Student.all.each do |student|
         puts "#{student.name.upcase}".colorize(:blue)
@@ -51,5 +51,5 @@ class CommandLineInterface
         puts "----------------------".colorize(:green)
       end
     end
-  
+
   end
